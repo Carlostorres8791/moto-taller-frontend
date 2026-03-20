4eb553cb151d35576138b411bf0037c723be7c85
Carlostorres8791 <escolta87@hotmail.com>
2026-03-19 22:01:20 -0500

    Implementación del Dashboard dináminco con datos reales

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Auth } from '../../services/auth';
 import { Navbar } from "../../component/navbar/navbar";
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Taller } from '../../services/taller';
 
 
 @Component({
@@ -11,12 +12,41 @@ import { RouterModule } from '@angular/router';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {
+export class Dashboard implements OnInit{
 
   usuario: any;
 
-  constructor(private auth: Auth){
+  talleres: any[] = [];
+
+  total = 0;
+  activos = 0;
+  inactivos = 0;
+
+  talleresInactivos: any[] = [];
+
+  constructor(private tallerServices: Taller, private auth: Auth) {}
+
+  ngOnInit(): void {
     this.usuario = this.auth.obtenerUsuario();
+    this.cargarDatos();
   }
 
+  cargarDatos() {
+    this.tallerServices.obtenerTalleres().subscribe({
+      next: (data) => {
+        this.talleres = data;
+
+        //KPIs
+        this.total = data.length;
+        this.activos = data.filter(t => t.activo).length;
+        this.inactivos = data.filter(t => !t.activo).length;
+
+        //tabla desactivados
+        this.talleresInactivos = data.filter(t => !t.activo);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }  
 }
